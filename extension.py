1b56450e6092091e7130f79193bd763488661c67
Jay Zhang <wangyoucao577@gmail.com>
2025-01-30 09:33:29 +0000

    fix: not await

--- extension.py
+++ extension.py
@@ -21,7 +21,7 @@ class DefaultAsyncLLMExtension(AsyncLLMBaseExtension):
         await super().on_start(ten_env)
 
         # initialize configuration
-        self.config = DefaultAsyncLLMConfig.create(ten_env=ten_env)
+        self.config = await DefaultAsyncLLMConfig.create_async(ten_env=ten_env)
         ten_env.log_info(f"config: {self.config}")
 
         """Implement this method to construct and start your resources."""
